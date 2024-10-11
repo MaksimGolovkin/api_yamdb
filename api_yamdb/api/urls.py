333db34f4c08fb3b18e84c1b9cf9d0d65fd565b0
Maksim Golovkin <maxmaxironfist@gmail.com>
2024-10-11 19:37:27 +0300

    old changes

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -14,10 +14,10 @@ v1_router.register('categories', CategoryViewSet, basename='categories')
 v1_router.register('genres', GenreViewSet, basename='genres')
 v1_router.register('titles', TitleViewSet, basename='titles')
 v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews',
+    r'titles/(?P<title_id>[^/.]+)/reviews', ReviewViewSet, basename='reviews',
 )
 v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    r'titles/(?P<title_id>[^/.]+)/reviews/(?P<review_id>[^/.]+)/comments',
     CommentViewSet,
     basename='comments',
 )
