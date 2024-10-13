445f13bf10083f2373742ac193a1a74c27802a39
Михаил Сайкин <sairin-mihail102@yandex.ru>
2024-10-14 02:06:12 +0400

    Добавлена админка

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, Genre, Title, Review, Comment
 from users.admin_mixins import GenreCategoryMixin
 from users.models import User
 
@@ -40,3 +40,37 @@ class TitleAdmin(admin.ModelAdmin):
     list_filter = ('year', 'category', 'genre')
     empty_value_display = '-пусто-'
     filter_horizontal = ('genre',)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Админская конфигурация для управления отзывами."""
+
+    list_display = (
+        'id',
+        'author',
+        'text',
+        'score',
+        'pub_date',
+        'title'
+    )
+    search_fields = ('author',)
+    list_filter = ('author', 'score', 'pub_date')
+    empty_value_display = '-пусто-'
+    
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Админская конфигурация для управления комментариями."""
+
+    list_display = (
+        'id',
+        'author',
+        'text',
+        'pub_date',
+        'review'
+    )
+    search_fields = ('author',)
+    list_filter = ('author', 'pub_date')
+    empty_value_display = '-пусто-'
+    
