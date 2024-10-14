d40df39111cbd4ace89cc6363a4fc9055ccde6a2
Maksim Golovkin <maxmaxironfist@gmail.com>
2024-10-15 00:34:46 +0300

    3

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -5,7 +5,7 @@ from api.constant import (
     USER, ADMIN, MODERATOR, WRONGUSERNAME,
     MAX_LEN_USERNAME, MAX_LEN_EMAIL
 )
-from api_yamdb.users.validators import user_name_validator
+from users.validators import user_name_validator
 
 
 ROLE_CHOICES = (
